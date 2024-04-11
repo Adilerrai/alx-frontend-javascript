7620e0c9a31bb56ed8298eb272accc74fac7a14b
Adil errai <67393790+Adilerrai@users.noreply.github.com>
2024-04-11 12:19:58 +0000

    Add files via upload

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
+  }
+
+  return newArray;
+}
